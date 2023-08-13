4ca4fd1a1a80d9763ad46681693a8c2194280032
Tyler McKenna <tmckenna1988@gmail.com>
2023-08-13 18:28:25 -0400

    Finished the login form styling

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -9,40 +9,53 @@ const Login = () => {
         console.log(formData);
     }
     return (
-        <form method="post" onSubmit={handleSubmit} className="form__login">
-            <label className="label__email">
-                Email:{" "}
-                <input
-                    name="input__email"
-                    placeholder="Enter your email address"
-                    className="input__text"
-                ></input>
-            </label>
-            <label className="label__password">
-                Password:{" "}
-                <input
-                    name="input__password"
-                    placeholder="Enter your password"
-                    className="input__text"
-                ></input>
-            </label>
-            <label className="label__rememberMe">
-                <input
-                    type="checkbox"
-                    name="input__rememberMe"
-                    defaultChecked={false}
-                    className="input__rememberMe"
-                ></input>
-                Remember me
-            </label>
-            <Button text="Sign in" className="button__sign_in" />
-            <Button
-                text="Sign in with Google"
-                className="button__sign_in_google"
-                img="/images/google.png"
-            />
-            <Button text="Sign in as Guest" className="button__sign_in_guest" />
-        </form>
+        <>
+            <form method="post" onSubmit={handleSubmit} className="form__login">
+                <label className="label__email">
+                    Email:{" "}
+                    <input
+                        type="email"
+                        name="input__email"
+                        placeholder="Enter your email address"
+                        className="input__text"
+                    ></input>
+                </label>
+                <label className="label__password">
+                    Password:{" "}
+                    <input
+                        type="password"
+                        name="input__password"
+                        placeholder="Enter your password"
+                        className="input__text"
+                    ></input>
+                </label>
+                <label className="label__rememberMe">
+                    <input
+                        type="checkbox"
+                        name="input__rememberMe"
+                        defaultChecked={false}
+                        className="input__rememberMe"
+                    ></input>
+                    Remember me
+                </label>
+                <Button text="Sign in" className="button__sign_in" />
+                <Button
+                    text="Sign in with Google"
+                    className="button__sign_in_google"
+                    img="/images/google.png"
+                />
+                <Button
+                    text="Sign in as Guest"
+                    className="button__sign_in_guest"
+                />
+            </form>
+            <div className="text__sign_up">
+                Don't have an account?{" "}
+                <a className="text__sign_up_link" href="/about">
+                    Sign up
+                </a>
+            </div>
+        </>
     );
 };
 
