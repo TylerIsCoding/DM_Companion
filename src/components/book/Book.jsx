6462fd900152dd301abe844f8dcef48e46c1b021
Tyler McKenna <tmckenna1988@gmail.com>
2023-08-12 20:10:18 -0400

    Added image to right page. Added login form

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -3,6 +3,7 @@ import Login from "../login/Login";
 import PageEndLeft from "../page_end/PageEndLeft";
 import PageEndRight from "../page_end/PageEndRight";
 import HeaderBook from "../header_book/HeaderBook";
+
 import "./book.css";
 
 const Book = () => {
@@ -11,10 +12,19 @@ const Book = () => {
             <div className="cover"></div>
             <PageEndLeft />
             <div className="container__page page__left">
-                <HeaderBook />
-                <Login />
+                <div className="content__page">
+                    <HeaderBook />
+                    <Login />
+                </div>
             </div>
-            <div className="container__page page__right"></div>
+            <div className="container__page page__right">
+                <div className="content__page">
+                    <h1 className="page__right__h1">
+                        Dungeon Master's<br></br> Companion
+                    </h1>
+                    <img src="/images/dragon.png" alt="dragon"></img>
+                </div>
+            </div>
             <PageEndRight />
         </div>
     );
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import './login.css'
+import React from "react";
+import "./login.css";
 
 const Login = () => {
-  function handleSubmit(e) {
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form)
-    console.log(formData)
-  }
-  return (
-    <form method='post' onSubmit={handleSubmit} className='form__login'>
-      <label className='label__email'>
-        Email: <input name="input__email" placeholder='Enter your email address'></input>
-      </label>
-      <label className='label__password'>
-        Password: <input name="input__password" placeholder='Enter your password'></input>
-      </label>
-      <label className='label__rememberMe'>
-        <input type="checkbox" name="input__rememberMe" defaultChecked={false}></input>Remember me
-      </label>
-    </form>
-  )
-}
+    function handleSubmit(e) {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        console.log(formData);
+    }
+    return (
+        <form method="post" onSubmit={handleSubmit} className="form__login">
+            <label className="label__email">
+                Email:{" "}
+                <input
+                    name="input__email"
+                    placeholder="Enter your email address"
+                    className="input__text"
+                ></input>
+            </label>
+            <label className="label__password">
+                Password:{" "}
+                <input
+                    name="input__password"
+                    placeholder="Enter your password"
+                    className="input__text"
+                ></input>
+            </label>
+            <label className="label__rememberMe">
+                <input
+                    type="checkbox"
+                    name="input__rememberMe"
+                    defaultChecked={false}
+                    className="input__rememberMe"
+                ></input>
+                Remember me
+            </label>
+        </form>
+    );
+};
 
-export default Login
+export default Login;
