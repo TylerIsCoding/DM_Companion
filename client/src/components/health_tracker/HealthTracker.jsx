c795bda9455e7ea9cb195e12a256709b39e78bde
Tyler McKenna <tmckenna1988@gmail.com>
2023-11-02 14:17:44 -0400

    Addition: Added the getEnemies API request to the server in order to populate the enemyArray state so we can generate a component for each enemy.

--- client/src/components/health_tracker/HealthTracker.jsx
+++ client/src/components/health_tracker/HealthTracker.jsx
@@ -1,10 +1,28 @@
 import { useState, useEffect } from "react";
+import axios from "../../api/axios";
 import HealthTrackerList from "./HealthTrackerList";
 import "./health_tracker.css";
 
 const HealthTracker = () => {
-    const [enemyArray, setEnemyArray] = useState([]);
+    const [enemyArray, setEnemyArray] = useState("");
     // const [addPage, setAddPage] = useState(false);
+
+    const getEnemies = async () => {
+        try {
+            const response = await axios.get("encounter/getEnemies", {
+                withCredentials: true,
+            });
+            if (response?.data) {
+                setEnemyArray(response.data.enemies || []);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    useEffect(() => {
+        getEnemies();
+    }, []);
 
     return (
         <section className="section__health_tracker">
